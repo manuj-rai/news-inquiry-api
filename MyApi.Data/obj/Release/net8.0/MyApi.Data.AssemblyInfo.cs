7560b888285be6d9445e80281c077e3d1802c42e
Manuj Rai <imanujrai7@gmail.com>
2025-01-27 12:50:14 +0530

    123

--- MyApi.Data/obj/Release/net8.0/MyApi.Data.AssemblyInfo.cs
+++ MyApi.Data/obj/Release/net8.0/MyApi.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApi.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89d0bd3724446a2834dd8b02e7054657551e96cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApi.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApi.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
